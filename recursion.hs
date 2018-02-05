b33c7644e18049ac4da37fc63487e3d451f8ca21
Miguel C. Canedo R <mi.canedo10@gmail.com>
2018-02-04 22:27:55 -0500

    Comentado y arreglado con especificacion de entrega 2.0

--- recursion.hs
+++ recursion.hs
@@ -11,21 +11,25 @@ Fecha: 04/02/18
 Descripción:
 		Representación de un tipo Árbol Binario cons sus respectivas hojas y nodos.
 -}
-data ArbolBinario = L | N1 ArbolBinario | N ArbolBinario ArbolBinario deriving(Eq, Show)
-
-crearArbolBinario :: Integer -> ArbolBinario
-crearArbolBinario 1 = L
-crearArbolBinario 2 = N1 L
-crearArbolBinario n = N (crearArbolBinario (n `div` 2)) (crearArbolBinario ((n-1) `div` 2)) 
+data ArbolBinario = L | N1 ArbolBinario | N2 ArbolBinario ArbolBinario deriving(Eq, Show)
 
 {-
 Descripción:
-		Función que calcula el tamaño de un Árbol Binario
+		Función que crea un Arbol Binario completo de 'n' niveles.
+-}
+crearArbolBinario :: Integer -> ArbolBinario
+crearArbolBinario 0 = L
+crearArbolBinario n = N2 (crearArbolBinario (n-1)) (crearArbolBinario (n-1))
+
+{-
+Descripción:
+		Función que calcula el tamaño de un Árbol Binario, es decir,
+		la cantidad de nodos que posee.
 -}
 tam :: ArbolBinario -> Integer
 tam L = 1
 tam (N1 t1) = 1 + tam t1
-tam (N t1 t2) = 1 + tam t1 + tam t2
+tam (N2 t1 t2) = 1 + tam t1 + tam t2
 
 {-
 Descripción:
@@ -34,15 +38,15 @@ Descripción:
 profundidad :: ArbolBinario -> Integer
 profundidad L = 1
 profundidad (N1 t1) = 1 + profundidad t1
-profundidad (N t1 t2) = 1 + max (profundidad t1) (profundidad t2)
+profundidad (N2 t1 t2) = 1 + max (profundidad t1) (profundidad t2)
 
 {-
 Descripción:
-		Función que crea un Árbol Binario.
+		Función que crea un Árbol Binario de exactamente 'n' nodos.
 -}
 hacerABinTree :: Integer -> ArbolBinario
-hacerABinTree 0 = error "No se puede crear un Árbol con 0 nodos"
-hacerABinTree 1 = crearArbolBinario 1
-hacerABinTree n = crearArbolBinario n
-
-
+hacerABinTree 1 = L
+hacerABinTree 2 = (N1 L)
+hacerABinTree n
+	| n <= 0 = error "Error. No se puede crear un Árbol con esa cantidad de nodos."
+	| n >= 3 = N2 (hacerABinTree (n `div` 2)) (hacerABinTree ((n-1) `div` 2))
